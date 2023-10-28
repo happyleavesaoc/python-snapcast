d1cdf377b8150d80a9f1b6d976b89dde3ddf87b8
Santiago Soto <81064015+SantiagoSotoC@users.noreply.github.com>
2023-10-28 12:45:12 -0300

    Update stream.py

--- snapcast/control/stream.py
+++ snapcast/control/stream.py
@@ -51,7 +51,7 @@ class Snapstream():
         """Get stream path."""
         return self._stream.get('uri').get('path')
     
-
+    @property
     def update(self, data):
         """Update stream."""
         self._stream = data
