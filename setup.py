f6bd2757e502bc11a87ab49d0f28d02e58b7c135
Alfredo Peña <15361812+arpena@users.noreply.github.com>
2023-11-24 15:58:38 -0300

    Restore setup.py named fields

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='snapcast',
     version='2.3.6',
     description='Control Snapcast.',
-    url='https://github.com/arpena/python-snapcast/',
+    url='https://github.com/happyleavesaoc/python-snapcast/',
     license='MIT',
     author='happyleaves',
-    author_email='arpena@gmail.com',
+    author_email='happyleaves.tfr@gmail.com',
     packages=['snapcast', 'snapcast.control', 'snapcast.client'],
     install_requires=[
         'construct>=2.5.2',
