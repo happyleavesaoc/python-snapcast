af1c479e380018997a888d29a7415ecba96c37f0
happyleaves <happyleaves.tfr@gmail.com>
2024-02-28 18:33:09 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snapcast',
-    version='2.3.3',
+    version='2.3.4',
     description='Control Snapcast.',
     url='https://github.com/happyleavesaoc/python-snapcast/',
     license='MIT',
