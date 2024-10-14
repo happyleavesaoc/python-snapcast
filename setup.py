5903636b4495266e7f75d009879811932bb717a4
Santiago Soto <santiago@soto.uy>
2024-10-14 17:47:56 -0300

    Update package name and repository URL

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='snapcast',
+    name='snapcast-mod',
     version='2.3.6',
     description='Control Snapcast.',
-    url='https://github.com/happyleavesaoc/python-snapcast/',
+    url='https://github.com/SantiagoSotoC/python-snapcast',
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
